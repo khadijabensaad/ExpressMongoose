5553cdd79e3ff76b567b86ced558b3a911c1fe0f
khadijabensaad <khadijabensaad4@gmail.com>
2024-04-06 02:38:57 +0100

    send to json

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/getAll", function (req, res, next) {
+  res.json("respond with a resource");
 });
-
+/* GET users listing by id. */
+router.get("/getAll/:id", function (req, res, next) {
+  res.json("recupére by id");
+});
+/* POST users listing. */
+router.post("/formation", function (req, res, next) {
+  res.json("add");
+});
+/* PUT users listing. */
+router.put("/formation/:id", function (req, res, next) {
+  res.json("update");
+});
+/* DELETE users listing. */
+router.delete("/formation", function (req, res, next) {
+  res.json("suppression");
+});
 module.exports = router;
